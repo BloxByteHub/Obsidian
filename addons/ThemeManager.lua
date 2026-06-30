77ba1abf48cc7b5c78ae7f132469b5850b29f5df
BloxByteHub <295589503+BloxByteHub@users.noreply.github.com>
2026-06-30 15:54:47 +0200

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -49,7 +49,7 @@ do
     ThemeManager.BuiltInThemes = {
         ["Default"] = {
             1,
-            { FontColor = "ffffff", MainColor = "E86320", AccentColor = "7d55ff", BackgroundColor = "0f0f0f", OutlineColor = "E86320", BackgroundImage = "" },
+            { FontColor = "ffffff", MainColor = "191919", AccentColor = "E86320", BackgroundColor = "0f0f0f", OutlineColor = "E86320", BackgroundImage = "" },
         },
         ["BBot"] = {
             2,
